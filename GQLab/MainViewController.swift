0dc52d683eeaaa952ef3ef8712b4a1ab09ef7c87
Ahmed Abdalla <ahmedd.abdallaa@gmail.com>
2021-09-01 02:00:34 -0400

    Add array variable to store data and populate table view with fake data

--- GQLab/MainViewController.swift
+++ GQLab/MainViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class MainViewController: UIViewController {
     
+    var searchResults = [String]()
+    
     @IBOutlet var searchBar: UISearchBar!
     @IBOutlet var tableView: UITableView!
 
@@ -27,18 +29,40 @@ class MainViewController: UIViewController {
 extension MainViewController: UISearchBarDelegate {
     /* UISearchBarDelegate method that is invoked when user taps the search button */
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        print("The search text is: '\(searchBar.text!)'")
+        
+        // Dismiss keyboard
+        searchBar.resignFirstResponder()
+        
+        // print("The search text is: '\(searchBar.text!)'")
+        searchResults = []
+        for i in 0...2 {
+            searchResults.append(
+                String(
+                    format: "Fake Result %d for '%@'", i, searchBar.text!
+                )
+            )
+        }
+        tableView.reloadData()
     }
 }
 
 // MARK: - Table View Delegate
 extension MainViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return searchResults.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cellIdentifier = "SearchResultCell"
+        
+        var cell: UITableViewCell! = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
+        
+        if cell == nil {
+            cell = UITableViewCell(style: .default, reuseIdentifier: cellIdentifier)
+        }
+        
+        cell.textLabel!.text = searchResults[indexPath.row]
+        return cell
     }
     
 }
